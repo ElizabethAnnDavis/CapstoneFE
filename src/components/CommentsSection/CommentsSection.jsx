a17793251c0e94bf21af2276a35d64e01f326cc7
ElizabethAnnDavis <immakitty88@gmail.com>
2025-02-25 04:24:13 -0500

    CommentsSection updated

--- src/components/CommentsSection/CommentsSection.jsx
+++ src/components/CommentsSection/CommentsSection.jsx
@@ -1,16 +1,56 @@
 //import './CommentsSection.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function CommentsSection(){
+export default function CommentsSection({ userId, favId }){
     const [comment, setComment] = useState("");
-    const [comments, setComments] = useState([]);
+    const [thisComment, setThisComment] = useState("");
 
-    const handleAdd = () => {
-        if(comment.trim() !== ""){
-            setComments([...comments, comment]);
-            setComment("");
+    useEffect(() => {
+        const getComment = async () => {
+            try{
+                const response = await fetch(`http://localhost:3000/${userId}/fav/${favId}`);
+                const data = await response.json();
+                setThisComment(data.comments || "");
+            }catch(err){
+                console.log(err.message);
+            }
+            getComment();
         }
-    };
+    }, [userId, favId])
+
+    const handleAdd = async () => {
+        if(comment.trim() !== ""){
+            try{
+                const response = await fetch(`http://localhost:3000/${userId}/fav/${favId}`, {
+                    method: 'PATCH',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({comments: comment})
+                });
+
+                const data = await response.json();
+                setThisComment(data.userProfile.favs.find(f => f.fav_id === favId).comments);
+                setComment("");
+            }catch(err){
+                console.log(err.message);
+            }
+        }
+    }
+
+    const handleDelete = async () => {
+        try{
+            const response = await fetch(`http://localhost:3000/${userId}/fav/${favId}`, {
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({comments: ""})
+            });
+
+            const data = await response.json();
+            setThisComment(data.userProfile.favs.find(f => f.fav_id === favId).comments);
+            setComment("");
+        }catch(err){
+            console.log(err.message);
+        }
+    }
 
     return(
         <div>
@@ -18,12 +58,13 @@ export default function CommentsSection(){
             <input type='text' value={comment} onChange={(e) => setComment(e.target.value)}/>
             <button onClick={handleAdd}>ADD</button>
             <div>
-                {comments.length > 0 ? (
-                    comments.map((comment, i) => (
-                        <div key={i}><p>{comment}</p></div>
-                    ))
+                {thisComment ? (
+                    <div >
+                        <p>{thisComment}</p>
+                        <button onClick={handleDelete}>Delete</button>
+                    </div>
                 ):(
-                    <p>Add your comments here</p>
+                    <p>Add your comment here</p>
                 )}
             </div>
         </div>
